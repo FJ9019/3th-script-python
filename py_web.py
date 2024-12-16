63323fc6d34b6a50e11b5dfcc4469ac14aac5349
FJ9019 <feenlus24@gmail.com>
2024-12-16 12:28:48 -0500

    First Save

---
+++ py_web.py
@@ -0,0 +1,17 @@
+#coding: utf-8
+
+age = 15
+
+if 0<=age<=5: #Bébé 0
+    print("Bienvenue dans l'interface !")
+elif 6<=age<=12: #Enfant 1
+    print("Bienvenue dans l'interface enfant")
+    
+elif 13<=age<=18: #Adolescent 2
+    print("Bienvenue dans l'interface adolescent")
+
+elif 19<=age<=50: #Adulte 4
+    print("Bienvenue dans l'interface adulte")
+
+else: #Senior 3
+    print("Bienvenue dans l'interface Senior !")
