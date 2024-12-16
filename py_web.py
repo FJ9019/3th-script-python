7d03efc6afa69082e8c268a590db176e01f45774
FJ9019 <feenlus24@gmail.com>
2024-12-16 13:56:33 -0500

    Update 2.0

--- py_web.py
+++ py_web.py
@@ -17,19 +17,34 @@ content = [
     ("La Ligne Rouge", "Terrence Malick", 4),
     ("Voyage au bout de l'enfer", "Michael Cimino", 4),
     ("Persuation", "Carrie Cracknell", 3),
-    ("Book od Love", "Analeine Cal y Mayor", 4)
+    ("Book od Love", "Analeine Cal y Mayor", 3)
 ]
 
 if 0<=age<=5: #Bébé 0
     print("Bienvenue dans l'interface Bébé !")
+    for item in content:
+        if item[2]<=0:
+            print(f"{item[0]} de {item[1]}")
 elif 6<=age<=12: #Enfant 1
     print("Bienvenue dans l'interface enfant !")
+    for item in content:
+        if item[2]<=1:
+            print(f"{item[0]} de {item[1]}")
     
 elif 13<=age<=18: #Adolescent 2
     print("Bienvenue dans l'interface adolescent !")
+    for item in content:
+        if item[2]<=2:
+            print(f"{item[0]} de {item[1]}")
 
 elif 19<=age<=50: #Adulte 4
     print("Bienvenue dans l'interface adulte !")
+    for item in content:
+        if item[2]<=4:
+            print(f"{item[0]} de {item[1]}")
 
 else: #Senior 3
     print("Bienvenue dans l'interface Senior !")
+    for item in content:
+        if item[2]<=3:
+            print(f"{item[0]} de {item[1]}")
